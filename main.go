e2e5f345a89b410a4bb4fba519d3b470756faf5e
muteeburrehman <muteeb285@gmail.com>
2024-12-12 18:09:33 +0500

    update api functionality added

--- main.go
+++ main.go
@@ -63,11 +63,22 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func updateMovie() {
+func updateMovie(w http.ResponseWriter, r *http.Request) {
 	//    set json content type
 	w.Header().Set("Content-Type", "application/json")
 	//params
 	params := mux.Vars(r)
+
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+		}
+	}
 
 }
 
